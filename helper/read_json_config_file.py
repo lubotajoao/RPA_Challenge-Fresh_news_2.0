7a8be329380dfacf3483936d06eafeb7d9f68e90
Lubota Ngola Joao <lubota4@gmail.com>
2024-06-23 16:30:46 -0300

    [feature] Reading Config File

---
+++ helper/read_json_config_file.py
@@ -0,0 +1,17 @@
+import json
+
+with open("config.json", "rb") as read_file:
+    configFile = json.load(read_file)
+
+# Folders
+base_path = configFile['BASE_PATH']
+input_folder_path = f"{base_path}/Input"
+output_folder_path = f"{base_path}/Output"
+report_folder_path = f"{output_folder_path}/Reports"
+log_csv_folder_path = f"{output_folder_path}/CSV Logs"
+
+# Files
+log_csv_file_name = configFile['LOG_CSV_DEFAULT_FILENAME']
+project_name = configFile['PROJECT_NAME']
+input_filename = configFile['INPUT_FILENAME']
+input_file_path = f"{input_folder_path}/{input_filename}.xlsx"
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from web.web_init_driver import init_driver
+from helper.read_json_config_file import input_folder_path, output_folder_path, report_folder_path, log_csv_folder_path,  log_csv_file_name, project_name, input_file_path
+from helper.config_dates import cur_day, cur_month, cur_year
 
 
 def main():
@@ -6,7 +8,25 @@ def main():
         # Starting Process
         print("The process has started.")
 
-        init_driver()
+        # Process Dates
+        current_day = cur_day()
+        current_moth = cur_month()
+        current_year = cur_year()
+
+        print(current_day, current_moth, current_year)
+
+        # Config file inputs
+        print(input_folder_path)
+        print(output_folder_path)
+        print(report_folder_path)
+        print(log_csv_folder_path)
+        print(log_csv_file_name)
+        print(project_name)
+        print(input_file_path)
+
+        # Creating Log and Process folder
+
+        # init_driver()
 
     except Exception as e:
         print(f'An error occurred in Orchestrator Module:\n[{e}]')
